2212eee20beee07749b66fb9d880baf17e43eec3
H <harley.paterson@hotmail.co.nz>
2019-11-15 00:59:34 +1300

    Skeleton files for the character stream system.

---
+++ char_stream.c
@@ -0,0 +1,11 @@
+/**
+ * @file char_stream.c
+ * 
+ * 'char_stream.c' implements character level access to a stream.
+ * 
+ * @author H Paterson.
+ * @copyright Boost Software License 1.0.
+ * @date 14/11/19.
+ */
+
+#include "char_stream.h"
---
+++ char_stream.h
@@ -0,0 +1,14 @@
+/**
+* @file char_stream.h
+*
+* 'char_stream.h' provides character level access to a stream.
+*
+* @author H Paterson.
+* @copyright Boost Software License 1.0.
+* @date 14/11/19.
+*/
+
+struct placeholder 
+{
+    int dummy;
+};
